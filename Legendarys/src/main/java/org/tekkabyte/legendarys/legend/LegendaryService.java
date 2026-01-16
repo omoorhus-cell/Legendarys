377c3a7720f75072e7586909e73c6cc0f157ced4
omoorhus-cell <omoorhus@gmail.com>
2026-01-16 18:53:20 -0500

    Delete Legendarys directory

--- Legendarys/src/main/java/org/tekkabyte/legendarys/legend/LegendaryService.java
+++
@@ -1,9 +0,0 @@
-package org.tekkabyte.legendarys.legend;
-
-import org.bukkit.inventory.ItemStack;
-
-public interface LegendaryService {
-    boolean isLegendary(ItemStack item);
-    String getLegendaryId(ItemStack item);
-    ItemStack applyLegendary(ItemStack base, String id);
-}
